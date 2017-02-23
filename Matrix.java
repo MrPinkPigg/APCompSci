c58bb9360612d5eeb32556f71234b34ef47739f3
Unknown <aaronknestaut@gmail.com>
2017-02-22 20:20:17 -0500

    Combined new and old matrix projects

--- Matrix.java
+++ Matrix.java
@@ -1,53 +1,123 @@
+/**
+ * Can perform various mathmatical matrix actions
+ * 
+ * @author Aaron Knestaut
+ */
+
+import java.util.*;
+
 public class Matrix {
-	int rowLength;
-	int colLength;
-	public Matrix (int row, int col) {
-		double [][] temp = new double [row][col];
-		rowLength = row;
-		colLength = col;
-	}
+    int rowLength;
+    int colLength;
+    /**
+     *
+     */
+    public Matrix (int row, int col) {
+        double [][] temp = new double [row][col];
+        rowLength = row;
+        colLength = col;
+    }
 
-	public void set (int row, int col, double input){}
+    /**
+     * 
+     */
+    public void set (int row, int col, double input){
+        
+    }
 
-	public double get (int row, int col){return 1;}
+    /**
+     * 
+     */
+    public double get (int row, int col){
+        return 1;
+    }
 
-	public Matrix sub (Matrix a, Matrix b) throws MatrixException{
-		int aRows = a.rowLength;
-		int aCols = a.colLength;
-		int bRows = b.rowLength;
-		int bCols = b.colLength;
+    /**
+     * 
+     */
+    public Matrix add (Matrix a, Matrix b) throws MatrixException{
+        int aRows = a.rowLength;
+        int aCols = a.colLength;
+        int bRows = b.rowLength;
+        int bCols = b.colLength;
 
-		if(a == null || b == null){
-			throw new MatrixException("Empty Matrix");
-		}
-		else if(aRows != bRows || aCols != bCols){
-			throw new MatrixException("Inequal Matricies");
-		}
+        if(a == null || b == null){
+            throw new MatrixException("Empty Matrix");
+        }
+        else if(aRows != bRows || aCols != bCols){
+            throw new MatrixException("Inequal Matricies");
+        }
 
-		Matrix dif = new Matrix(aRows, aCols);
-		double answ;
+        Matrix sum = new Matrix(aRows, aCols);
+        double answ;
 
-		for(int x = 0; x <= aRows; x++){
-			for(int y = 0; y <= aCols; y++){
-				answ = a.get(x, y) - b.get(x, y);
-				dif.set(x, y, answ);
-			}
-		}
+        for(int x = 0; x <= aRows; x++){
+            for(int y = 0; y <= aCols; y++){
+                answ = a.get(x, y) + b.get(x, y);
+                sum.set(x, y, answ);
+            }
+        }
 
-		return dif;
-	}
+        return sum;
+    }
 
-	/**
-	* MatrixException
-	* This exception is thrown for any error associated with the
-	* methods in the Matrix class.
-	*
-	* @author Mr. Kramer
-	* @version 10/3/2010
-	*/
-	public class MatrixException extends Exception{
-		public MatrixException() { super(); }
+    /**
+     * 
+     */
+    public Matrix sub (Matrix a, Matrix b) throws MatrixException{
+        int aRows = a.rowLength;
+        int aCols = a.colLength;
+        int bRows = b.rowLength;
+        int bCols = b.colLength;
 
-		public MatrixException (String s) { super (s); }
-	}
+        if(a == null || b == null){
+            throw new MatrixException("Empty Matrix");
+        }
+        else if(aRows != bRows || aCols != bCols){
+            throw new MatrixException("Inequal Matricies");
+        }
+
+        Matrix dif = new Matrix(aRows, aCols);
+        double answ;
+
+        for(int x = 0; x <= aRows; x++){
+            for(int y = 0; y <= aCols; y++){
+                answ = a.get(x, y) - b.get(x, y);
+                dif.set(x, y, answ);
+            }
+        }
+
+        return dif;
+    }
+
+    /**
+     * 
+     */
+    /*public Matrix mult (Matrix a, Matrix b){
+
+    }*/
+
+    /**
+     * 
+     */
+    /*public Matrix transpose (Matrix a){
+
+    }*/
+
+    /**
+     * MatrixException
+     * This exception is thrown for any error associated with the
+     * methods in the Matrix class.
+     * 
+     * @author Mr. Kramer
+     * 
+     * @version 10/3/2010
+     */
+
+    public class MatrixException extends Exception
+    {
+        public MatrixException() { super(); }
+
+        public MatrixException (String s) { super (s); }
+    }
 }
