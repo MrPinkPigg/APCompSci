bf3ee2ed9c09ae708006e285f000c514fc22e303
aaron <aaronknestaut@gmail.com>
2017-02-24 11:00:05 -0500

    Finished set and get, worked on transpose

--- Matrix.java
+++ Matrix.java
@@ -10,30 +10,43 @@ public class Matrix {
     int rowLength;
     int colLength;
     /**
-     *
+     * [Matrix description]
+     * @param  row [description]
+     * @param  col [description]
+     * @return     [description]
      */
     public Matrix (int row, int col) {
-        double [][] temp = new double [row][col];
+        double [][] matrix = new double [row][col];
         rowLength = row;
         colLength = col;
     }
 
     /**
-     * 
+     * [set description]
+     * @param row   [description]
+     * @param col   [description]
+     * @param input [description]
      */
     public void set (int row, int col, double input){
-        
+        matrix[row][col]  = input;
     }
 
     /**
-     * 
+     * [get description]
+     * @param  row [description]
+     * @param  col [description]
+     * @return     [description]
      */
     public double get (int row, int col){
-        return 1;
+        return matrix[row][col];
     }
 
     /**
-     * 
+     * [add description]
+     * @param  a               [description]
+     * @param  b               [description]
+     * @return                 [description]
+     * @throws MatrixException [description]
      */
     public Matrix add (Matrix a, Matrix b) throws MatrixException{
         int aRows = a.rowLength;
@@ -62,7 +75,11 @@ public class Matrix {
     }
 
     /**
-     * 
+     * [sub description]
+     * @param  a               [description]
+     * @param  b               [description]
+     * @return                 [description]
+     * @throws MatrixException [description]
      */
     public Matrix sub (Matrix a, Matrix b) throws MatrixException{
         int aRows = a.rowLength;
@@ -91,7 +108,11 @@ public class Matrix {
     }
 
     /**
-     * 
+     * [mult description]
+     * @param  a               [description]
+     * @param  b               [description]
+     * @return                 [description]
+     * @throws MatrixException [description]
      */
     public Matrix mult (Matrix a, Matrix b) throws MatrixException{
         int aRows = a.rowLength;
@@ -102,7 +123,7 @@ public class Matrix {
         if(a == null || b == null){
             throw new MatrixException("Empty Matrix");
         }
-        else if(aRows != bCols || aCols != bRows){
+        else if(){
             throw new MatrixException("Inequal Matricies");
         }
         
@@ -119,11 +140,26 @@ public class Matrix {
     }
 
     /**
-     * 
+     * [transpose description]
+     * @param  a               [description]
+     * @return                 [description]
+     * @throws MatrixException [description]
      */
-    /*public Matrix transpose (Matrix a){
+    public Matrix transpose (Matrix a) throws MatrixException{
+    	int aRows = a.rowLength;
+        int aCols = a.colLength;
 
-    }*/
+        if(a == null){
+            throw new MatrixException("Empty Matrix");
+        }
+
+        Matrix trans = new Matrix(aCols, aRows);
+        double move;
+
+        
+
+        return trans;
+    }
 
     /**
      * MatrixException
