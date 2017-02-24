a2f00bb785adbb0d735859a3c01159935b45c2c3
aaron <aaronknestaut@gmail.com>
2017-02-24 12:28:41 -0500

    Wrote transpose method, worked on mult methods, created a tester, and made MatrixException into its own class

--- Matrix.java
+++ Matrix.java
@@ -9,46 +9,46 @@ import java.util.*;
 public class Matrix {
     int rowLength;
     int colLength;
+    double [][] matrix;
     /**
-     * [Matrix description]
-     * @param  row [description]
-     * @param  col [description]
-     * @return     [description]
+     * Constructor that creates matricies
+     * @param  row number of rows
+     * @param  col number of cols
      */
     public Matrix (int row, int col) {
-        double [][] matrix = new double [row][col];
+        matrix = new double [row][col];
         rowLength = row;
         colLength = col;
     }
 
     /**
-     * [set description]
-     * @param row   [description]
-     * @param col   [description]
-     * @param input [description]
+     * sets a value in a matrix
+     * @param row   what row
+     * @param col   what col
+     * @param input value to input into (row, col)
      */
     public void set (int row, int col, double input){
-        matrix[row][col]  = input;
+        matrix[row][col] = input;
     }
 
     /**
-     * [get description]
-     * @param  row [description]
-     * @param  col [description]
-     * @return     [description]
+     * gets a value in a matrix
+     * @param  row what row
+     * @param  col what col
+     * @return     returns the value at (row, col)
      */
     public double get (int row, int col){
         return matrix[row][col];
     }
 
     /**
-     * [add description]
-     * @param  a               [description]
-     * @param  b               [description]
-     * @return                 [description]
-     * @throws MatrixException [description]
+     * adds two matricies together
+     * @param  a               first matrix
+     * @param  b               second matrix
+     * @return                 matrix of sums
+     * @throws MatrixException if the matricies are empty or not equal
      */
-    public Matrix add (Matrix a, Matrix b) throws MatrixException{
+    public static Matrix add (Matrix a, Matrix b) throws MatrixException{
         int aRows = a.rowLength;
         int aCols = a.colLength;
         int bRows = b.rowLength;
@@ -64,8 +64,8 @@ public class Matrix {
         Matrix sum = new Matrix(aRows, aCols);
         double answ;
 
-        for(int x = 0; x <= aRows; x++){
-            for(int y = 0; y <= aCols; y++){
+        for(int x = 0; x < aRows; x++){
+            for(int y = 0; y < aCols; y++){
                 answ = a.get(x, y) + b.get(x, y);
                 sum.set(x, y, answ);
             }
@@ -75,13 +75,13 @@ public class Matrix {
     }
 
     /**
-     * [sub description]
-     * @param  a               [description]
-     * @param  b               [description]
-     * @return                 [description]
-     * @throws MatrixException [description]
+     * subtracts two matricies
+     * @param  a               first matrix
+     * @param  b               second matrix
+     * @return                 matrix of differences
+     * @throws MatrixException if matricies are empty or not equal
      */
-    public Matrix sub (Matrix a, Matrix b) throws MatrixException{
+    public static Matrix sub (Matrix a, Matrix b) throws MatrixException{
         int aRows = a.rowLength;
         int aCols = a.colLength;
         int bRows = b.rowLength;
@@ -97,8 +97,8 @@ public class Matrix {
         Matrix dif = new Matrix(aRows, aCols);
         double answ;
 
-        for(int x = 0; x <= aRows; x++){
-            for(int y = 0; y <= aCols; y++){
+        for(int x = 0; x < aRows; x++){
+            for(int y = 0; y < aCols; y++){
                 answ = a.get(x, y) - b.get(x, y);
                 dif.set(x, y, answ);
             }
@@ -108,13 +108,13 @@ public class Matrix {
     }
 
     /**
-     * [mult description]
-     * @param  a               [description]
-     * @param  b               [description]
-     * @return                 [description]
-     * @throws MatrixException [description]
+     * multiplies a matrix by a matrix
+     * @param  a               first matrix
+     * @param  b               second matrix
+     * @return                 matrix of products
+     * @throws MatrixException if matricies are empty 
      */
-    public Matrix mult (Matrix a, Matrix b) throws MatrixException{
+    public static Matrix mult (Matrix a, Matrix b) throws MatrixException{
         int aRows = a.rowLength;
         int aCols = a.colLength;
         int bRows = b.rowLength;
@@ -123,16 +123,21 @@ public class Matrix {
         if(a == null || b == null){
             throw new MatrixException("Empty Matrix");
         }
-        else if(){
-            throw new MatrixException("Inequal Matricies");
+        else if(aRows != bCols || aCols != bRows){
+            throw new MatrixException("Cannot Multiply Matricies");
         }
         
         Matrix prod = new Matrix(aRows, bCols);
         double answ;
+        double sum = 0;
         
-        for(int x = 0; x <= aRows; x++){
-            for(int y = 0; y <= bCols; y++){
-                
+        for(int x = 0; x < aRows; x++){
+            for(int y = 0; y < bCols; y++){
+                for(int z = 0; z < aCols; z++){
+                	answ = a.get(x, z) * b.get(z, y);
+                	sum = sum + answ;
+                	prod.set(x, y, sum);
+                }
             }
         }
         
@@ -140,12 +145,40 @@ public class Matrix {
     }
 
     /**
-     * [transpose description]
-     * @param  a               [description]
-     * @return                 [description]
-     * @throws MatrixException [description]
+     * multiplies a matrix by a set value
+     * @param  a               matrix
+     * @param  b               value
+     * @return                 matirx of porducts
+     * @throws MatrixException if matrix is empty
      */
-    public Matrix transpose (Matrix a) throws MatrixException{
+    public static Matrix mult (Matrix a, double b) throws MatrixException{
+        int aRows = a.rowLength;
+        int aCols = a.colLength;
+
+        if(a == null){
+            throw new MatrixException("Empty Matrix");
+        }
+        
+        Matrix prod = new Matrix(aRows, aCols);
+        double answ;
+        
+        for(int x = 0; x < aRows; x++){
+            for(int y = 0; y < aCols; y++){
+                answ = a.get(x, y) * b;
+                prod.set(x, y, answ);
+            }
+        }
+        
+        return prod;
+    }
+
+    /**
+     * transposes a matrix
+     * @param  a               matrix
+     * @return                 transposed matrix
+     * @throws MatrixException if matrix is empty
+     */
+    public static Matrix transpose (Matrix a) throws MatrixException{
     	int aRows = a.rowLength;
         int aCols = a.colLength;
 
@@ -156,25 +189,12 @@ public class Matrix {
         Matrix trans = new Matrix(aCols, aRows);
         double move;
 
-        
+        for(int x = 0; x < aRows; x++){
+            for(int y = 0; y < aCols; y++){
+                trans.set(y, x, a.get(x, y));
+            }
+        }
 
         return trans;
-    }
-
-    /**
-     * MatrixException
-     * This exception is thrown for any error associated with the
-     * methods in the Matrix class.
-     * 
-     * @author Mr. Kramer
-     * 
-     * @version 10/3/2010
-     */
-
-    public class MatrixException extends Exception
-    {
-        public MatrixException() { super(); }
-
-        public MatrixException (String s) { super (s); }
     }
 }
