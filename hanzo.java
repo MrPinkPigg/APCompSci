1507bb56b37b17a0ebe82c3cae3d7394a5f4983c
MrPinkPigg <aaronknestaut@gmail.com>
2017-05-31 18:44:19 -0400

    finished hanzo

--- hanzo.java
+++ hanzo.java
@@ -7,6 +7,7 @@
 
 package defense;
 import robocode.*;
+import java.awt.Color;
 
 /**
  * Hanzo - a robot by Aaron
@@ -17,24 +18,24 @@ public class Hanzo extends AdvancedRobot
 	 * run: Hanzo's default behavior
 	 */
 	public void run() {
+		setBodyColor(Color.white);
+		setGunColor(Color.orange);
+		setRadarColor(Color.black);
+		setScanColor(Color.blue);
+		setBulletColor(Color.gray);
 		// Robot main loop
 		while(true) {
-			if(getRoundNum() <= 6) {
-				double distance = Math.random()*150;
-            	double angle = Math.random()*45;
-            	turnRight(angle);
-            	ahead(distance);
-            	ahead(50);
-            	turnGunRight(360);
-            	back(50);
-            	turnGunRight(360);
-        	}
-        	else {
-        		ahead(90);
-				turnGunRight(360);
-				back(70);
-				turnGunRight(360);
-        	}
+				double distance = Math.random()*200;
+            	double angle = Math.random()*100;
+            	if(Math.random() > .5) {
+            		setTurnRight(angle);
+            	}
+            	else {
+            		setTurnLeft(angle);
+            	}
+            	setAhead(distance);
+            	setTurnGunRight(360);
+            	execute();
 		}
 	}
 
@@ -43,11 +44,11 @@ public class Hanzo extends AdvancedRobot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
 		double distance = e.getDistance(); //get the distance of the scanned robot
-    	if(distance > 650)
-    		fire(2);
-    	else if(distance > 350 && distance <= 650)
+    	if(distance > 300 && distance <= 450)
+        	fire(2);
+   		else if(distance > 40 && distance <= 300)
         	fire(3);
-   		else if(distance <= 350)
+        else if(distance <= 40)
         	fire(5);
 	}
 
@@ -57,21 +58,30 @@ public class Hanzo extends AdvancedRobot
 	public void onHitByBullet(HitByBulletEvent e) {
 		double bearing = e.getBearing(); //Get the direction which is arrived the bullet.
     	if(getEnergy() < 60) { // if the energy is low, the robot go away from the enemy
-        	turnRight(-bearing); 
-        	ahead(100); 
+        	setTurnRight(-bearing); 
+        	setAhead(100); 
     	}
-    	else {
-    		turnLeft(360);
+    	else { //if energy is good, searches for the robot that shot it to fire back
+    		turnRight(bearing);
+    		setTurnGunRight(bearing);
     	}
+    	execute();
 	}
 	
 	/**
 	 * onHitWall: What to do when you hit a wall
 	 */
-	public void onHitWall(HitWallEvent e) {
+	public void onHitWall(HitWallEvent e) { //turns away from the wall and moves away
 		double bearing = e.getBearing(); //get the bearing of the wall
-    	turnRight(-bearing);
-    	ahead(80);
+    	setTurnRight(-bearing); 
+    	setAhead(90);
+    	execute();
 	}	
+
+	/**
+	 * onRobotDeath: If another robot dies
+	 */
+	public void onRobotDeath(RobotDeathEvent e) {
+		System.out.println("SIMPLE GEOMETRY");
+	}
 }
-								
